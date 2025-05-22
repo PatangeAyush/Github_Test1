a301f1689077b95c2e4407dee8d2fe8f558a596c
PatangeAyush <patangeayush977@gmail.com>
2025-05-22 15:16:49 +0530

    Trial 4

---
+++ DapperAPI.DTOs/Trial4.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperAPI.DTOs
+{
+    internal class Trial4
+    {
+        public int Product_ID { get; set; }
+        public string Product_Name { get; set; } = string.Empty;
+        public string? Product_Icon { get; set; }
+        public string? Product_Code { get; set; }
+        public int MasterCategory_ID { get; set; }
+        public string? Category_Name { get; set; }
+    }
+}
